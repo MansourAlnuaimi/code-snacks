591648bb3f2b1f3917eb4b72121306db28157e2e
Mansour Alnuaimi <100427822+MansourAlnuaimi@users.noreply.github.com>
2022-09-16 16:58:04 +0400

    added pasword generator tool

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['*', '@', '#', '$', '%', '^', '&', '_', '+', '=']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+           'k', 'l', 'm','n','o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+animals = ['cat', 'lion', 'dog', 'fish', 'cow', 'bull', 'goat', 'ant', 'fly', 'ape', 'frog', 'crab', 'bee']
+adjectives = ['huge', 'tiny', 'red', 'gold', 'green', 'ugly', 'greedy', 'evil', 'good', 'nice', 'happy', 'sad', 'angry']
+stop = 'text'
+while stop != '0':
+    password = random.choice(letters).upper() + random.choice(letters).lower() + random.choice(numbers) \
+               + random.choice(numbers) + '_' + random.choice(adjectives) + '_' + random.choice(animals) \
+               + '_' + random.choice(numbers).lower() + random.choice(letters).upper() + 2*random.choice(symbols)
+    print(f"\nYour password is: {password}")
+    stop = input("Enter any key for another password, enter 0 to stop.  ")
