0382a6a1328907f11b4c7911e5f8555d9b67e1e5
Mansour Alnuaimi <100427822+MansourAlnuaimi@users.noreply.github.com>
2022-10-04 21:07:38 +0400

    Update
    - better code.
    - password's length can be specified by the user

--- password_generator.py
+++ password_generator.py
@@ -1,15 +1,37 @@
-# to try it online, go to https://onlinegdb.com/c7LP2p61qr
 import random
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['*', '@', '#', '$', '%', '^', '&', '_', '+', '=']
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-           'k', 'l', 'm','n','o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-animals = ['cat', 'lion', 'dog', 'fish', 'cow', 'bull', 'goat', 'ant', 'fly', 'ape', 'frog', 'crab', 'bee']
-adjectives = ['huge', 'tiny', 'red', 'gold', 'blue', 'ugly', 'fast', 'evil', 'good', 'nice', 'fat', 'sad', 'fool']
-stop = 'text'
-while stop != '0':
-    password = random.choice(letters).upper() + random.choice(letters).lower() + random.choice(numbers) \
-               + random.choice(numbers) + '_' + random.choice(adjectives) + '_' + random.choice(animals) \
-               + '_' + random.choice(numbers).lower() + random.choice(letters).upper() + 2*random.choice(symbols)
-    print(f"\nYour password is: {password}")
-    stop = input("Enter any key for another password, enter 0 to stop.  ")
+
+
+def main():
+    # initialize an array to store password's characters
+    password = []
+    # ask the user for the length
+    length = int(input('Password length: '))
+    # iterate through the loop 'length' times.
+    for i in range(length):
+        # add to the password's array a random generated character by the function
+        password.append(random_char())
+
+    # print the password array, join the characters using .join() method to display it as single string,
+    # '' is a seperator between elements
+    print(f"Password: {''.join(password)}")
+
+
+def random_char():
+    # arrays to store characters
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['*', '@', '#', '$', '%', '^', '&', '_', '+', '=']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+               'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    # a variable that stores an integer in the range [1, 4]
+    chooser = random.randint(1, 4)
+    # look for the chooser's value, return a random character from certain array based on it
+    match chooser:
+        case 1: return random.choice(numbers)
+        case 2: return random.choice(symbols)
+        case 3: return random.choice(letters)
+        case 4: return random.choice(letters).upper()
+
+
+# Generate passwords infinitely
+while True: main()
